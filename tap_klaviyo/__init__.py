fb8439fdfa34ec82e0a14368818ed492b0303ae6
Ryan Bell <ryan@fixdapp.com>
2021-09-08 10:05:41 -0400

    Comment out currently unneeded functions

--- tap_klaviyo/__init__.py
+++ tap_klaviyo/__init__.py
@@ -7,12 +7,12 @@ from singer import metadata
 from tap_klaviyo.utils import get_incremental_pull, get_full_pulls, get_all_pages, get_incremental_pull_additional_properties
 
 ENDPOINTS = {
-    'global_exclusions': 'https://a.klaviyo.com/api/v1/people/exclusions',
+    #'global_exclusions': 'https://a.klaviyo.com/api/v1/people/exclusions',
     'lists': 'https://a.klaviyo.com/api/v1/lists',
     # to get list of available metrics
-    'metrics': 'https://a.klaviyo.com/api/v1/metrics',
+    #'metrics': 'https://a.klaviyo.com/api/v1/metrics',
     # to get individual metric data
-    'metric': 'https://a.klaviyo.com/api/v1/metric/',
+    #'metric': 'https://a.klaviyo.com/api/v1/metric/',
     # to get list members
     'list_members': 'https://a.klaviyo.com/api/v2/group/{list_id}/members/all'
 }
@@ -32,21 +32,21 @@ EVENT_MAPPINGS = {
     "Cancelled Order": "cancelled_order"
 }
 
-# ADDITIONAL_PROPERTIES = {
-#     "placed_order": {
-#         "attributed_flow_value",
-#         "attributed_flow_count",
-#         "carthook_funnel_id_value",
-#         "carthook_funnel_id_count",
-#     }
-# }
+ADDITIONAL_PROPERTIES = {
+    "placed_order": {
+        "attributed_flow_value",
+        "attributed_flow_count",
+        "carthook_funnel_id_value",
+        "carthook_funnel_id_count",
+    }
+}
 
-# ADDITIONAL_PROPERTIES_KEYS = {
-#     "attributed_flow_value",
-#     "attributed_flow_count",
-#     "carthook_funnel_id_value",
-#     "carthook_funnel_id_count"
-# }
+ADDITIONAL_PROPERTIES_KEYS = {
+    "attributed_flow_value",
+    "attributed_flow_count",
+    "carthook_funnel_id_value",
+    "carthook_funnel_id_count"
+}
 
 logger = singer.get_logger()
 
@@ -127,7 +127,6 @@ def do_sync(config, state, catalog):
     list_ids = config.get('list_ids')
     start_date = config['start_date'] if 'start_date' in config else None
     stream_ids_to_sync = set()
-    logger.info('Did we get here??')
     for stream in catalog.get('streams'):
         mdata = metadata.to_map(stream['metadata'])
         if metadata.get(mdata, (), 'selected'):
@@ -159,38 +158,37 @@ def do_sync(config, state, catalog):
             get_full_pulls(stream, ENDPOINTS[stream['stream']], api_key)
 
 
-def get_available_metrics(api_key):
-    metric_streams = []
-    for response in get_all_pages('metric_list',
-                                  ENDPOINTS['metrics'], api_key):
-        for metric in response.json().get('data'):
-            if metric['name'] in EVENT_MAPPINGS:
-                metric_streams.append(
-                    Stream(
-                        stream=EVENT_MAPPINGS[metric['name']],
-                        tap_stream_id=metric['id'],
-                        key_properties="id",
-                        puller='INCREMENTAL'
-                    )
-                )
-                if EVENT_MAPPINGS[metric['name']] in ADDITIONAL_PROPERTIES:
-                     for additional_property in ADDITIONAL_PROPERTIES[EVENT_MAPPINGS[metric['name']]]:
-                        logger.info("attributed endpoint " + additional_property)
-                        metric_streams.append(
-                            Stream(
-                                stream=additional_property,
-                                tap_stream_id=metric['id'],
-                                key_properties="id",
-                                puller='INCREMENTAL'
-                            )
-                        )
-    return metric_streams
+# def get_available_metrics(api_key):
+#     metric_streams = []
+#     for response in get_all_pages('metric_list',
+#                                   ENDPOINTS['metrics'], api_key):
+#         for metric in response.json().get('data'):
+#             if metric['name'] in EVENT_MAPPINGS:
+#                 metric_streams.append(
+#                     Stream(
+#                         stream=EVENT_MAPPINGS[metric['name']],
+#                         tap_stream_id=metric['id'],
+#                         key_properties="id",
+#                         puller='INCREMENTAL'
+#                     )
+#                 )
+#                 if EVENT_MAPPINGS[metric['name']] in ADDITIONAL_PROPERTIES:
+#                      for additional_property in ADDITIONAL_PROPERTIES[EVENT_MAPPINGS[metric['name']]]:
+#                         logger.info("attributed endpoint " + additional_property)
+#                         metric_streams.append(
+#                             Stream(
+#                                 stream=additional_property,
+#                                 tap_stream_id=metric['id'],
+#                                 key_properties="id",
+#                                 puller='INCREMENTAL'
+#                             )
+#                         )
+#     return metric_streams
 
 
 def discover(api_key):
-    metric_streams = get_available_metrics(api_key)
     return {"streams": [a.to_catalog_dict()
-                        for a in metric_streams + FULL_STREAMS]}
+                        for a in FULL_STREAMS]}
 
 
 def do_discover(api_key):
